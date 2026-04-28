24018e4ee057e1704ef44cbca94342bec96be01f
Nada-Rashed-445 <445052014@std.psau.edu.sa>
2026-04-29 02:33:52 +0300

    Similar synchronization for method runToCompletion

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -210,6 +210,9 @@ class Process implements Runnable {
     
     public void runToCompletion() {
         // TODO: Similar synchronization needed here
+        try{
+            SharedResources.cpuSemaphore.acquire();
+             
         try {
             System.out.println(Colors.BRIGHT_CYAN + "  ⚡ " + Colors.BOLD + Colors.CYAN + name + 
                               Colors.RESET + Colors.BRIGHT_CYAN + " is the last process, running to completion" + 
@@ -225,8 +228,12 @@ class Process implements Runnable {
             System.out.println(Colors.BRIGHT_GREEN + "  ✓ " + Colors.BOLD + Colors.CYAN + name + 
                               Colors.RESET + Colors.BRIGHT_GREEN + " finished execution!" + Colors.RESET);
             System.out.println();
-        } catch (InterruptedException e) {
-            System.out.println(Colors.RED + "  ✗ " + name + " was interrupted." + Colors.RESET);
+
+        } finally{
+            SharedResources.cpuSemaphore.release();
+        }
+    }catch (InterruptedException e) {
+            System.out.println(Colors.RED + "  ✗ " + name + " semaphore interrupted during runToCompletion." + Colors.RESET);
         }
     }
     
